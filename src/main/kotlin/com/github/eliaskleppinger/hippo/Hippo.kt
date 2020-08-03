8eebd6a51520a5e7b6b0e5055aac42f1943f538d
Elias Kleppinger <eliaskleppinger@web.de>
2020-08-03 15:56:25 +0200

    Setted up project structure

---
+++ src/main/kotlin/com/github/eliaskleppinger/hippo/Hippo.kt
@@ -0,0 +1,11 @@
+package com.github.eliaskleppinger.hippo
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Hippo : JavaPlugin() {
+
+    override fun onEnable() {
+        print("Test :D")
+    }
+
+}
